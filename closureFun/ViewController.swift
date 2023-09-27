a6bb32ef00811fe7718eb98c76d09c8c6d99e24e
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2023-09-27 09:24:37 -0500

    Initial Commit

---
+++ closureFun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  closureFun
+//
+//  Created by ANDREW KAISER on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
